af9b15b0fb768782ceb9fe27a5c099e57362b392
denev <65989857+deneb0723@users.noreply.github.com>
2020-08-16 18:04:36 +0900

    modify

--- src/js/bg.js
+++ src/js/bg.js
@@ -4,7 +4,7 @@ const IMG_NUMBER = 7;
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `../src/img/${imgNumber + 1}`;
+  image.src = `../src/img/${imgNumber + 1}.jpg`;
   image.classList.add("bgImage");
   body.prepend(image);
 }
